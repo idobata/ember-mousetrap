f0e94eb6fcc781d17971cfe73d5b6107d123cdce
Keita Urashima <ursm@ursm.jp>
2014-12-25 00:05:56 +0900

    Use Ember.on() instead of overriding init()

--- dist/amd/mixins/mousetrap-route.js
+++ dist/amd/mixins/mousetrap-route.js
@@ -7,36 +7,32 @@ define(
     __exports__["default"] = Ember.Mixin.create({
       mergedProperties: ['shortcuts'],
 
-      init: function() {
-        this._super.apply(this, arguments);
+      mousetrapBindKeys: Ember.on('activate', function() {
+        if (!this.shortcuts) return;
 
-        this.on('activate', this, function() {
-          if (!this.shortcuts) return;
+        Ember.keys(this.shortcuts).forEach(function(key) {
+          var callback = this.shortcuts[key];
 
-          Ember.keys(this.shortcuts).forEach(function(key) {
-            var callback = this.shortcuts[key];
+          if (!callback.__ember_mousetrap__) return;
 
-            if (!callback.__ember_mousetrap__) return;
+          var var$0 = callback.__ember_mousetrap__, keys = var$0.keys, action = var$0.action;
 
-            var var$0 = callback.__ember_mousetrap__, keys = var$0.keys, action = var$0.action;
+          Mousetrap.bind(keys, callback.bind(this), action);
+        }, this);
+      }),
 
-            Mousetrap.bind(keys, callback.bind(this), action);
-          }, this);
-        });
+      mousetrapUnbindKeys: Ember.on('deactivate', function() {
+        if (!this.shortcuts) return;
 
-        this.on('deactivate', this, function() {
-          if (!this.shortcuts) return;
+        Ember.keys(this.shortcuts).forEach(function(key) {
+          var callback = this.shortcuts[key];
 
-          Ember.keys(this.shortcuts).forEach(function(key) {
-            var callback = this.shortcuts[key];
+          if (!callback.__ember_mousetrap__) return;
 
-            if (!callback.__ember_mousetrap__) return;
+          var keys = callback.__ember_mousetrap__.keys;
 
-            var keys = callback.__ember_mousetrap__.keys;
-
-            Mousetrap.unbind(keys);
-          }, this);
-        });
-      }
+          Mousetrap.unbind(keys);
+        }, this);
+      })
     });
   });
