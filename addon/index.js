c2fe3068679dee64218a3f821baf6404a85fcb63
Vishesh Joshi <vishesh@yahoo-inc.com>
2015-05-01 14:21:01 -0700

    Adding View in index.js

--- addon/index.js
+++ addon/index.js
@@ -1,4 +1,5 @@
 import MousetrapRoute from 'ember-mousetrap/mixins/mousetrap-route';
+import MousetrapView from 'ember-mousetrap/mixins/mousetrap-view';
 import mousetrap from 'ember-mousetrap/macros/mousetrap';
 
-export { MousetrapRoute, mousetrap };
+export { MousetrapRoute, MousetrapView, mousetrap };
