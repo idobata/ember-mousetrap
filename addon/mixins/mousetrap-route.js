d4b5a7c2af4f0db61f42f22c92697ed4c9eb93e8
FilmKnurd <andrew@andrewelster.com>
2016-11-22 15:00:10 -0500

    Guards against FastBoot global instead of service

--- addon/mixins/mousetrap-route.js
+++ addon/mixins/mousetrap-route.js
@@ -1,11 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Mixin.create({
-  fastboot: Ember.inject.service(),
   mergedProperties: ['shortcuts'],
 
   mousetrapBindKeys: Ember.on('activate', function() {
-    if (this.get('fastboot.isFastBoot') || !this.shortcuts) { return; }
+    if (typeof FastBoot !== undefined || !this.shortcuts) { return; }
 
     Object.keys(this.shortcuts).forEach(function(key) {
       var callback = this.shortcuts[key];
@@ -20,7 +19,7 @@ export default Ember.Mixin.create({
   }),
 
   mousetrapUnbindKeys: Ember.on('deactivate', function() {
-    if (this.get('fastboot.isFastBoot') || !this.shortcuts) { return; }
+    if (typeof FastBoot !== undefined || !this.shortcuts) { return; }
 
     Object.keys(this.shortcuts).forEach(function(key) {
       var callback = this.shortcuts[key];
