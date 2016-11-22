56e7038d121dddc6428af7a944aba9e2e9d1969a
FilmKnurd <andrew@andrewelster.com>
2016-11-22 12:28:56 -0500

    Fastboot compatibility
    - Does not load the mousetrap library into the fastboot build
    - Skips binding keys if running inside fastboot
    - Skips unbinding keys if running inside fastboot

--- addon/mixins/mousetrap-route.js
+++ addon/mixins/mousetrap-route.js
@@ -1,10 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Mixin.create({
+  fastboot: Ember.inject.service(),
   mergedProperties: ['shortcuts'],
 
   mousetrapBindKeys: Ember.on('activate', function() {
-    if (!this.shortcuts) { return; }
+    if (this.get('fastboot.isFastBoot') || !this.shortcuts) { return; }
 
     Object.keys(this.shortcuts).forEach(function(key) {
       var callback = this.shortcuts[key];
@@ -19,7 +20,7 @@ export default Ember.Mixin.create({
   }),
 
   mousetrapUnbindKeys: Ember.on('deactivate', function() {
-    if (!this.shortcuts) { return; }
+    if (this.get('fastboot.isFastBoot') || !this.shortcuts) { return; }
 
     Object.keys(this.shortcuts).forEach(function(key) {
       var callback = this.shortcuts[key];
--- index.js
+++ index.js
@@ -7,6 +7,8 @@ module.exports = {
   included: function(app) {
     this._super.included(app);
 
-    app.import(app.bowerDirectory + '/mousetrap/mousetrap.js');
+    if (!process.env.EMBER_CLI_FASTBOOT) {
+      app.import(app.bowerDirectory + '/mousetrap/mousetrap.js');
+    }
   }
 };
